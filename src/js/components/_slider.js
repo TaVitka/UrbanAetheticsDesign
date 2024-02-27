f7746973ba4f5ec281e3f78bb8fad1c8615ecfbd
TaVitka <tavitka17@gmail.com>
2024-02-27 16:59:16 +0200

    added hero and services section to services page

--- src/js/components/_slider.js
+++ src/js/components/_slider.js
@@ -17,3 +17,25 @@ const swiperProjects = new Swiper('.projects__slider', {
     prevEl: '.btn__items .btn--prev',
   },
 });
+
+// links from <a> to target slide
+window.addEventListener('DOMContentLoaded', function(){
+
+  function linkToSlide(slider) {
+
+    let slideId = window.location.hash.substring(1);
+    let targetSlide = document.getElementById(slideId);
+
+    if (targetSlide === null) {
+      return;
+    }
+
+    let slidesArr = Array.from(targetSlide.parentElement.children);
+    let indexOfTargetSlide = slidesArr.indexOf(targetSlide);
+
+    slider.slideTo(indexOfTargetSlide);
+  }
+
+
+  linkToSlide(swiperProjects);
+});
