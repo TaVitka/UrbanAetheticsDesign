16a2d61ceff7eaeebe53abbc900f35cd9d961905
TaVitka <tavitka17@gmail.com>
2024-03-03 15:52:48 +0200

    added fixes

--- src/js/components/_slider.js
+++ src/js/components/_slider.js
@@ -2,9 +2,8 @@ import Swiper, { Navigation, Pagination, EffectFade, Scrollbar } from 'swiper';
 
 const swiperProjects = new Swiper('.projects__slider', {
   slidesPerView: "auto",
-  spaceBetween: 0,
+  spaceBetween: 10,
   centeredSlides: true,
-  centeredSlidesBounds: true,
   speed: 1000,
   modules: [Navigation, Scrollbar, Pagination, EffectFade],
   pagination: {
@@ -18,38 +17,34 @@ const swiperProjects = new Swiper('.projects__slider', {
     nextEl: '.btn__items .btn--next',
     prevEl: '.btn__items .btn--prev',
   },
-  breakpoints: {
-    1180: {
-      slidesPerView: 1,
-    },
-  },
 });
 
-function initializeSwiper(initialSlideIndex, swiperSlider) {
-  var swiper = new Swiper(swiperSlider, {
-    initialSlide: initialSlideIndex,
-  });
-}
-
-// links from <a> to target slide
-window.addEventListener('DOMContentLoaded', function(){
-
-  function linkToSlide(slider, sliderClassName) {
-
-    let slideId = window.location.hash.substring(1);
-    let targetSlide = document.getElementById(slideId);
-
-    if (targetSlide === null) {
-      return;
-    }
-
-    let slidesArr = Array.from(targetSlide.parentElement.children);
-    let indexOfTargetSlide = slidesArr.indexOf(targetSlide);
-
-    initializeSwiper(indexOfTargetSlide, sliderClassName);
-  }
-
-
-
-  linkToSlide(swiperProjects, '.projects__slider');
-});
+// function initializeSwiper(initialSlideIndex, swiperSlider) {
+//   var swiper = new Swiper(swiperSlider, {
+//     initialSlide: initialSlideIndex,
+//     centeredSlides: true,
+//   });
+// }
+//
+// // links from <a> to target slide
+// window.addEventListener('DOMContentLoaded', function(){
+//
+//   function linkToSlide(slider, sliderClassName) {
+//
+//     let slideId = window.location.hash.substring(1);
+//     let targetSlide = document.getElementById(slideId);
+//
+//     if (targetSlide === null) {
+//       return;
+//     }
+//
+//     let slidesArr = Array.from(targetSlide.parentElement.children);
+//     let indexOfTargetSlide = slidesArr.indexOf(targetSlide);
+//
+//     initializeSwiper(indexOfTargetSlide, sliderClassName);
+//   }
+//
+//
+//
+//   linkToSlide(swiperProjects, '.projects__slider');
+// });
